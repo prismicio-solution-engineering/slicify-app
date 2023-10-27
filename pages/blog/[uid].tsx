cd18394dd91d01e60f02726a26e7ab70c0dc910d
Dianka Soumaré <48419929+Dmsooja@users.noreply.github.com>
2023-10-27 11:35:40 +0200

    Initialize graphtype

---
+++ pages/blog/[uid].tsx
@@ -0,0 +1,151 @@
+import Head from "next/head";
+import type { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { createClient } from "@/prismicio";
+import { Content } from "@prismicio/client";
+import { SliceZone } from "@prismicio/react";
+import * as prismicH from "@prismicio/helpers";
+import { components as mktComponents } from "@/slices/marketing";
+import { components as blogComponents } from "@/slices/blog";
+import { authorGraphQuery, blogArticleGraphQuery } from "@/utils/graphQueries";
+import { getLanguages } from "@/utils/getLanguages";
+import BlogLayout from "@/components/BlogLayout";
+
+type BlogArticleProps = InferGetStaticPropsType<typeof getStaticProps>;
+type PageParams = { uid: string };
+
+export default function BlogArticle({
+  page,
+  author,
+  header,
+  footer,
+  languages,
+}: BlogArticleProps) {
+  return (
+    <>
+      <Head>
+        <title>{page.data.meta_title || "Slicify - Blog Article"}</title>
+        <meta
+          name="description"
+          content={
+            page.data.meta_title ||
+            "Slicify, nice article to understand slices for everyone."
+          }
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <BlogLayout
+        header={header.data}
+        footer={footer.data}
+        languages={languages}
+        author={author}
+        page={page}
+      >
+        <SliceZone
+          slices={page.data.slices}
+          components={{ ...mktComponents, ...blogComponents }}
+        />
+      </BlogLayout>
+    </>
+  );
+}
+
+export async function getStaticProps({
+  locales,
+  locale,
+  previewData,
+  params,
+}: GetStaticPropsContext<PageParams>) {
+  const client = createClient({ previewData });
+  //    ^ Automatically contains references to document types
+
+  if (params && params.uid) {
+    const page =
+      //    ^ Typed as BlogIndexDocument
+      await client.getByUID<Content.BlogArticleDocument>(
+        "blog_article",
+        params.uid,
+        { lang: locale }
+      );
+
+    const linkedAuthor =
+      //    ^ Typed as BlogArticleDocument
+      await client.getByUID<Content.BlogArticleDocument>(
+        "blog_article",
+        params.uid,
+        {
+          lang: locale,
+          graphQuery: authorGraphQuery,
+        }
+      );
+
+    const linkedBlogArticles =
+      await client.getByUID<Content.BlogArticleDocument>(
+        //    ^ Typed as BlogArticleDocument
+        "blog_article",
+        params.uid,
+        {
+          lang: locale,
+          graphQuery: blogArticleGraphQuery,
+        }
+      );
+
+    let index = 0;
+
+    const pageWithArticles = {
+      ...page,
+      data: {
+        ...page.data,
+        slices: page?.data?.slices?.map((slice) => {
+          if (slice.slice_type === "article_list") {
+            index++;
+            return {
+              ...linkedBlogArticles?.data?.slices[index - 1],
+            };
+          }
+          return {
+            ...slice,
+          };
+        }),
+      },
+    };
+
+    const header = await client.getSingle<Content.HeaderDocument>("header", {
+      lang: locale,
+    });
+    //    ^ Typed as HeaderDocument
+
+    const footer = await client.getSingle<Content.FooterDocument>("footer", {
+      lang: locale,
+    });
+    //    ^ Typed as FooterDocument
+
+    const languages = await getLanguages(page, client, locales);
+
+    if (page) {
+      return {
+        props: {
+          page: pageWithArticles,
+          author: linkedAuthor,
+          header,
+          footer,
+          languages,
+        },
+      };
+    }
+  }
+
+  return {
+    notFound: true,
+  };
+}
+
+// Paths
+export async function getStaticPaths() {
+  const client = createClient();
+  const documents = await client.getAllByType("blog_article", { lang: "*" });
+  return {
+    paths: documents.map((page) => `${prismicH.asLink(page)}`),
+    fallback: false, // if a page has already been generated but doesn't show => display the cached page
+  };
+}
---
+++ utils/graphQueries.ts
@@ -0,0 +1,115 @@
+import { Content } from "@prismicio/client"
+import * as prismicT from "@prismicio/types"
+import * as prismicH from "@prismicio/helpers"
+import { AuthorDocument, BlogArticleDocument, BlogCategoryDocument } from "@/prismicio-types";
+
+export const isOfTypeBlogArticleDocument= 
+  (contentRelationshipField: prismicT.ContentRelationshipField | undefined): contentRelationshipField is prismicT.ContentRelationshipField & {
+  data?: BlogArticleDocument["data"]
+} | undefined =>  {
+  return (
+    prismicH.isFilled.contentRelationship(contentRelationshipField) &&
+    typeof contentRelationshipField.data === "object" &&
+    contentRelationshipField.data !== null &&
+    "featured_image" in contentRelationshipField.data
+  );
+};
+
+export const isOfTypeBlogCategoryDocument= 
+  (contentRelationshipField: prismicT.ContentRelationshipField | undefined): contentRelationshipField is prismicT.ContentRelationshipField & {
+  data?: BlogCategoryDocument["data"]
+} | undefined =>  {
+  return (
+    prismicH.isFilled.contentRelationship(contentRelationshipField) &&
+    typeof contentRelationshipField.data === "object" &&
+    contentRelationshipField.data !== null &&
+    "category_name" in contentRelationshipField.data
+  );
+};
+
+export const isOfTypeAuthorDocument= 
+  (contentRelationshipField: prismicT.ContentRelationshipField | undefined): contentRelationshipField is prismicT.ContentRelationshipField & {
+  data?: AuthorDocument["data"]
+} | undefined =>  {
+  return (
+    prismicH.isFilled.contentRelationship(contentRelationshipField) &&
+    typeof contentRelationshipField.data === "object" &&
+    contentRelationshipField.data !== null &&
+    "author_image" in contentRelationshipField.data &&
+    "author_name" in contentRelationshipField.data &&
+    "author_role" in contentRelationshipField.data
+  );
+};
+
+export const authorGraphQuery = `
+{
+  blog_article {
+    author {
+      ...on author {
+        ...authorFields
+      }
+    }
+  }
+}
+`;
+
+export const blogArticleGraphQuery = `
+{
+  blog_article {
+    slices {
+      ... on article_list {
+        variation {
+          ... on verticalList {
+            primary {
+              ...primaryFields
+            }
+            items {
+              article {
+                ...on blog_article {
+                  title
+                  excerpt
+                  featured_image
+                  author {
+                    ...on author {
+                      ...authorFields
+                    }
+                  }
+                  category {
+                    ...on blog_category {
+                      ...blog_categoryFields
+                    }
+                  }
+                }
+              }
+            }
+          }
+          ... on horizontalList {
+            primary {
+              ...primaryFields
+            }
+            items {
+              article {
+                ...on blog_article {
+                  title
+                  excerpt
+                  featured_image
+                  author {
+                    ...on author {
+                      ...authorFields
+                    }
+                  }
+                  category {
+                    ...on blog_category {
+                      ...blog_categoryFields
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
