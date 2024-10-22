c533474dd17a30a1591034f5b90c7575406ba6ac
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2024-10-22 12:17:17 +0200

    force cache and prismic tag

--- prismicio.tsx
+++ prismicio.tsx
@@ -40,8 +40,8 @@ export const createClient = (config = {}) => {
   const client = prismic.createClient(repositoryName, {
     routes,
     fetchOptions:
-      process.env.NODE_ENV === "production"
-        ? { next: { revalidate: 60 } }
+      process.env.NODE_ENV === 'production'
+        ? { next: { tags: ['prismic'] }, cache: 'force-cache' }
         : { next: { revalidate: 5 } },
     ...config,
   });
