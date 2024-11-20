868ef713d8d6aa5b15c131d2c3eb13a6b69cc911
Dmsooja <dev@sooja.fr>
2024-11-20 18:09:28 +0100

    Refactor theme

--- slices/marketing/Hero/index.tsx
+++ slices/marketing/Hero/index.tsx
@@ -1,60 +1,22 @@
 import type { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { PrismicRichText } from "@prismicio/react";
-import Image from "next/image";
 import { Button } from "@/components/Button";
 import { Container } from "@/components/Container";
 import { UnderlineDoodle } from "@/components/UnderlineDoodle";
-import bgBlue from "@/images/background-blue.jpg";
-import bgLight from "@/images/background-light.jpg";
+import { ThemeContainer } from "@/components/Theme";
 
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
-const BackgroundImage = ({
-  theme,
-}: {
-  theme: "Blue" | "Dark" | "Light" | "White";
-}) => {
-  if (theme === "Blue") {
-    return (
-      <Image
-        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-        src={bgBlue}
-        alt=""
-        width={2245}
-        height={1636}
-        unoptimized
-      />
-    );
-  }
-  if (theme === "Light") {
-    return (
-      <Image
-        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-        src={bgLight}
-        alt=""
-        width={2245}
-        height={1636}
-        unoptimized
-      />
-    );
-  }
-};
 export default function Hero({ slice }: HeroProps) {
-  const theme = slice.primary.theme;
-
-  const themeColor = theme === "Blue" || theme === "Dark" ? "dark" : "light";
+  const themeColor =
+    slice.primary.theme === "Blue" || slice.primary.theme === "Dark"
+      ? "dark"
+      : "light";
 
   return (
     <section id={slice.primary.anchor || undefined}>
-      <div
-        className={`relative isolate overflow-hidden ${
-          themeColor === "dark" ? "bg-dark-blue" : "bg-white"
-        }`}
-      >
-        {(theme === "Blue" || theme === "Light") && (
-          <BackgroundImage theme={theme} />
-        )}
+      <ThemeContainer theme={slice.primary.theme}>
         <Container className="pb-16 pt-20 text-center lg:pt-32">
           <PrismicRichText
             field={slice.primary.title}
@@ -62,7 +24,7 @@ export default function Hero({ slice }: HeroProps) {
               heading1: ({ children }) => (
                 <h1
                   className={`mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight ${
-                    themeColor === "dark" ? "text-white" : "text-slate-900"
+                    themeColor === "dark" ? "text-white" : "text-dark-gray"
                   } sm:text-7xl`}
                 >
                   {children}
@@ -100,7 +62,7 @@ export default function Hero({ slice }: HeroProps) {
                   paragraph: ({ children }) => (
                     <p
                       className={`mx-auto mt-6 max-w-2xl text-lg tracking-tight ${
-                        themeColor === "dark" ? "text-white" : "text-slate-700"
+                        themeColor === "dark" ? "text-white" : "text-dark-gray"
                       }`}
                     >
                       {children}
@@ -123,11 +85,7 @@ export default function Hero({ slice }: HeroProps) {
                     <Button key={idx} field={item.cta_link} variant="outline">
                       <svg
                         aria-hidden="true"
-                        className={`h-3 w-3 flex-none ${
-                          themeColor === "dark"
-                            ? "fill-light-blue"
-                            : "fill-dark-blue"
-                        } group-active:fill-current`}
+                        className="h-3 w-3 flex-none fill-light-blue group-active:fill-current"
                       >
                         <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
                       </svg>
@@ -158,7 +116,7 @@ export default function Hero({ slice }: HeroProps) {
             </>
           )}
         </Container>
-      </div>
+      </ThemeContainer>
     </section>
   );
 }
